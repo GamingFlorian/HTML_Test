701be36fb8b57fb1ff1df14a74a36c67123d680a
GamingFlorian <GamingFlorian@t-online.de>
2016-02-03 19:19:55 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ console.log("Die Webseite wurde gestartet");
 
 var Click = 0;
 
-function Button(){
+function TestButton(){
   Click++;
   document.getElementById("Button").value=Click + "x geklickt";
 }

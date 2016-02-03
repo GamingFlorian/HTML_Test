20628eca191de3b55a21f4682507726ef1a1447b
GamingFlorian <GamingFlorian@t-online.de>
2016-02-03 20:56:56 +0100

    Update umfrage.js

--- umfrage.js
+++ umfrage.js
@@ -1,6 +1,11 @@
 function Button1(){
-  if(document.getElementById("Button1").value == "Thema 1"){
-    console.log("Update 1: " + document.getElementById("Button1").value);
-    document.getElementById("Button1").value = "Teil 1";
+  if(document.getElementById("Button1").value == "Finde deinen Buchstaben"){
+    document.getElementById("Thema").innerHTML = "Wo bist du lieber?";
+    document.getElementById("Button1").value = "Oben";
+  }else if(document.getElementById("Thema").innerHTML == "Wo bist du lieber?"){
+    document.getElementById("Thema").innerHTML = "Welche Seite?";
+    document.getElementById("Button1").value = "Rechts";
+  }else if(document.getElementById("Thema").innnerHTML == "Welche Seite?"){
+    document.getElementById("Thema").innerHTML = "Du bist A!";
   }
 }

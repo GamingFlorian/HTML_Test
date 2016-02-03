ba795e78fb257295a59f39c0e652c226c58c7c21
GamingFlorian <GamingFlorian@t-online.de>
2016-02-03 20:32:43 +0100

    Update umfrage.js

--- umfrage.js
+++ umfrage.js
@@ -1,1 +1,16 @@
 console.log("Webseite gestartet");
+
+function Button1(){
+  if(document.getElementById("Button1").value = "Thema 1"){
+    console.log("Thema 1");
+    document.getElementById("Button1").value = "Teil 1";
+  }
+}
+
+function Button2(){
+  
+}
+
+function Button3(){
+  
+}

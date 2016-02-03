9316bbb05cac4e9497ce90d73e08849f88ee9315
GamingFlorian <GamingFlorian@t-online.de>
2016-02-03 20:45:48 +0100

    Update umfrage.js

--- umfrage.js
+++ umfrage.js
@@ -1,5 +1,5 @@
 function Button1(){
-  if(document.getElementById("Button1").value == "Thema1"){
+  if(document.getElementById("Button1").value = "Thema1"){
     console.log("Update 1: " + document.getElementById("Button1").value);
     document.getElementById("Button1").value = "Teil 1";
   }
